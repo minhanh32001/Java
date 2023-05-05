4f053a1acc94d34e16cac89313846c1fe5dd77dd
minhanh3001 <nongmanhbh2001@gmail.com>
2023-05-05 11:18:09 +0700

    update new api for product, problem at database, going to change from H2 to MySQL

--- ShellPhone/src/main/java/com/project/ShellPhone/controllers/CartController.java
+++
@@ -1,2 +0,0 @@
-package com.project.ShellPhone.controllers;public class CartController {
-}
--- ShellPhone/src/main/java/com/project/ShellPhone/models/Cart.java
+++ ShellPhone/src/main/java/com/project/ShellPhone/models/Cart.java
@@ -1,2 +1,34 @@
-package com.project.ShellPhone.models;public class Cart {
+package com.project.ShellPhone.models;
+
+import jakarta.persistence.Entity;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Product> cart;
+
+    public Cart() {
+        this.cart = new ArrayList<Product>();
+    }
+    protected void showCart(){
+        for (Product product: this.cart){
+            product.toString();
+        }
+
+    }
+    protected void addProduct(Product product){
+        this.cart.add(product);
+    }
+    protected void remove(Product product) {
+        this.cart.remove(product);
+    }
+    public double getPrice(){
+        double sumPrice = 0;
+        for (Product product: this.cart){
+            sumPrice += product.getPrice();
+        }
+        return sumPrice;
+    }
 }
