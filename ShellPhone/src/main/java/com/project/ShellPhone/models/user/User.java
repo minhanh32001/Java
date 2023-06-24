9b5b630f512f23765712816046b72a6dee94e338
minhanh3001 <nongmanhbh2001@gmail.com>
2023-06-25 02:28:07 +0700

    role resolve

--- ShellPhone/src/main/java/com/project/ShellPhone/models/user/User.java
+++ ShellPhone/src/main/java/com/project/ShellPhone/models/user/User.java
@@ -46,6 +46,8 @@ public class User implements UserDetails {
                 this.isAdmin = true;
                 break;
             }
+            else
+                this.isAdmin = false;
         }
     }
 
